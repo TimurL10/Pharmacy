0dc8149c18c49d881d092fdc89fada8d7b811d8e
Timur Lumelsky <Lumelsky@hotmail.com>
2020-02-19 20:04:59 +0300

    writing db

---
+++ DB/CatalogContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkWithFarmacy.Models;
+
+namespace WorkWithFarmacy.DB
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Preorder> Preorders { get; set; }
+        public DbSet<OrderHeaderToStore> OrderHeadersList { get; set; }
+        public DbSet<OrderRowToStore> OrderRowsList { get; set; }
+        public DbSet<OrderStatusToStore> OrderStatusesList { get; set; }
+    }
+}
